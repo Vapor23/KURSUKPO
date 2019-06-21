2371ea3bd7e839f34430edb58a69b708b524c862
Vapor23 <51885868+Vapor23@users.noreply.github.com>
2019-06-22 00:43:12 +0300

    new tests #4

--- test/java/TreeTest.java
+++ test/java/TreeTest.java
@@ -10,7 +10,47 @@ public class TreeTest {
         Assert.assertEquals(1, t1.mRoot.mValue);
     }
 
- 
+     @Test
+    public void testDefaultConstructor() {
+        Tree t1 = new Tree();
+        Assert.assertNull(t1.mRoot);
+    }
+
+    @Test
+    public void testIntegerConstructor() {
+        Tree t1 = new Tree(1);
+        Assert.assertNotNull(t1.mRoot);
+    }
+    
+
+    @Test
+    public void testEqualTreesEqualHashCodes() {
+        Tree t1 = new Tree(10);
+        t1.insert(2, 12);
+        Tree t2 = new Tree(10);
+        t2.insert(2, 12);
+        Assert.assertEquals(t1.hashCode(), t2.hashCode());
+    }   
+
+
+    @Test
+    public void testToStringEmpty() {
+        Tree t1 = new Tree();
+        Assert.assertEquals("[]", t1.toString());
+    }
+
+    @Test
+    public void testToStringSingleNode() {
+        Tree t1 = new Tree(1);
+        Assert.assertEquals("[1]", t1.toString());
+    }
+
+    @Test
+    public void testToStringManyNodes() {
+        Tree t1 = new Tree(1);
+        t1.insert(12, 56, 7, 2, 1);
+        Assert.assertEquals("[1, 1, 2, 7, 12, 56]", t1.toString());
+    }
 
     @Test
     public void testSingleRotateLeft() {
